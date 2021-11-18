bbacc2e077bfcd0ec896b7f9dd3417ee5b9bbc02
amalrnambiar <amalrnambiarlive@gmail.com>
2021-11-18 15:10:07 +0100

    callback url protocol bug fix

--- app/helpers/google_sign_in/url_helper.rb
+++ app/helpers/google_sign_in/url_helper.rb
@@ -1,5 +1,7 @@
 module GoogleSignIn::UrlHelper
   def callback_url
-    GoogleSignIn::Engine.routes.url_helpers.callback_url(host: request.host_with_port)
+    GoogleSignIn::Engine.routes.url_helpers.callback_url(
+      host: request.host_with_port, protocol: request.protocol
+    )
   end
 end
