928968e125d5bed9061ad44b895bad6595991d48
amalrnambiar <amalrnambiarlive@gmail.com>
2021-11-29 16:26:18 +0100

    bug fix

--- lib/google_sign_in/redirect_protector.rb
+++ lib/google_sign_in/redirect_protector.rb
@@ -10,7 +10,7 @@ module GoogleSignIn
 
     def ensure_same_origin(target, source)
       if target.blank? || (target =~ QUALIFIED_URL_PATTERN && origin_of(target) != host_with_port(source))
-        raise Violation, "Redirect target #{target.inspect} does not have same origin as request (expected #{host_with_port(source)})"
+        raise Violation, "Redirect target #{origin_of(target)} does not have same origin as request (expected #{host_with_port(source)})"
       end
     end
 
